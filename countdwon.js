0fd82d0d9bb3076db4c7cc432ca635c4a1062e6e
invitacionesweb <invitacionesweb.info@gmail.com>
2026-08-01 19:34:34 -0300

    a
    a

--- countdwon.js
+++ countdwon.js
@@ -1,5 +1,5 @@
 // Establecer la fecha de destino
-const fechaDestino = new Date("2026-09-19T23:59:59");
+const fechaDestino = new Date("2026-09-12T23:59:59");
 
 // Variable para almacenar el ID del intervalo
 let intervalo;
